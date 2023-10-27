9d6dc406bc27ce3cab828b49015a0c69d798b2e9
Isaac Lal <isaaclal124@gmail.com>
2023-10-27 16:05:11 -0400

    configured cors and express route

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -1,39 +1,10 @@
-import express from 'express';
-import { PORT, MongoDB_URL } from './config.js';
-import { Book } from './models/bookModel.js';
-import mongoose from 'mongoose';
-import booksRoute from './routes/booksRoute.js';
-import cors from 'cors';
-
-const app = express();
-
-// Middleware for parsing request body
-app.use(express.json());
-
-// Middleware for handling CORS policy
-// Option 1: Allow all origins with default of cors(*)
-// app.use(cors());
-// Option 2: Allow custom irigins
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
-
-// HTTP Route
-app.get('/', (req, res) => {
-  console.log(req);
-  return res.status(234).send('Bookstore');
-});
-
-app.use('/books', booksRoute);
-
-// Node.js routes without express
-/*
 // Route for saving a new book
-app.post('/books', async (req, res) => {
+import express from 'express';
+import { Book } from '../models/bookModel.js';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(400).send({
@@ -58,7 +29,7 @@ app.post('/books', async (req, res) => {
 });
 
 // Route for getting all books from mongo database
-app.get('/books', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const books = await Book.find({});
 
@@ -73,7 +44,7 @@ app.get('/books', async (req, res) => {
 });
 
 // Route for getting one book from database by id
-app.get('/books/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const book = await Book.findById(id);
@@ -86,7 +57,7 @@ app.get('/books/:id', async (req, res) => {
 });
 
 // Route for updating a book
-app.put('/books/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     if (!req.body.title || !req.body.author || !req.body.publishYear) {
       return res.status(400).send({
@@ -110,7 +81,7 @@ app.put('/books/:id', async (req, res) => {
 });
 
 // Route for deleting a book
-app.delete('/books/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
 
@@ -126,19 +97,5 @@ app.delete('/books/:id', async (req, res) => {
     res.status(500).send({ message: err.message });
   }
 });
-*/
 
-// Connect to MongoDB database
-mongoose
-  .connect(MongoDB_URL)
-  .then(() => {
-    console.log('App connected to database');
-
-    // Listen on port
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.log(err);
-  });
+export default router;
