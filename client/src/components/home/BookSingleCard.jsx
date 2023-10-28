2da6c979ee2da1d61652ada7d8891755cdf4c69c
Isaac Lal <isaaclal124@gmail.com>
2023-10-28 14:18:15 -0400

    completed createbook component

--- client/src/components/home/BookSingleCard.jsx
+++ client/src/components/home/BookSingleCard.jsx
@@ -12,9 +12,6 @@ const BookSingleCard = ({ book }) => {
 
   return (
     <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relatie hover:shadow-xl'>
-      <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>
-        {book.publishYear}
-      </h2>
       <h4 className='my-2 text-gray-500'>{book._id}</h4>
       <div className='flex justify-start items-center gap-x-2'>
         <PiBookOpenTextLight className='text-red-300 text-2xl' />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
-import BooksTable from '../components/home/BooksTable';
 import BooksCard from '../components/home/BooksCard';
 
 const Home = () => {
@@ -30,28 +29,15 @@ const Home = () => {
 
   return (
     <div className='p-4'>
-      <div className='flex justify-center items-center gap-x-4'>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('table')}>
-          Table
-        </button>
-        <button
-          className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
-          onClick={() => setShowType('card')}>
-          Card
-        </button>
-      </div>
+      
       <div className='flex justify-between items-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
-        <Link to='books/create'>
+        <Link to='/books/create'>
           <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
       {loading ? (
         <Spinner />
-      ) : showType === 'table' ? (
-        <BooksTable books={books} />
       ) : (
         <BooksCard books={books} />
       )}
